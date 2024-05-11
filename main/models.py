334c20f55bd63f3c071705525459965e093ef43c
GeisonBrito <geisonbrito@icloud.com>
2024-05-10 21:38:56 -0300

    acressimo de tabelas no models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+
+class Item(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    Todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    text = models.TextField(max_length=255)
+    complete = models.BooleanField(default=False)
